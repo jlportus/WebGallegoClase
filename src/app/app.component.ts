12b728f1a52b50284a17bf1ae0821595f95f434e
jlportus <jl_puerto@hotmail.com>
2021-04-21 11:04:11 +0200

    Creado el proyecto y añadida libreria bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'WebPortus2';
+  title = environment.title;
 }
