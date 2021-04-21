0555a9d5c1edd83f16733f0c69fa4e5f351cab25
jlportus <jl_puerto@hotmail.com>
2021-04-21 11:16:53 +0200

    Creados componentes sin enrutar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShellComponent } from './core/shell/shell.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ``,
+    component: ShellComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
