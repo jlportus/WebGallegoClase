8da49a5e6bbe8f7c52c60c9ec4c3721f39d02157
JLPP <jl_puerto@hotmail.com>
2021-04-25 13:04:58 +0200

    Estructuras de control *ngIf y *ngFor

--- src/app/contacts/contacts/contacts.component.ts
+++ src/app/contacts/contacts/contacts.component.ts
@@ -10,12 +10,22 @@ export class ContactsComponent implements OnInit {
   public description = "Manage your contact list";
   public numContacts = 0;
   public counterClass = "tag secondary";
-  public formHidden = true;
+  public formHidden = false;
+
+  public workStatuses = [
+    { id: 0, description: "unknow" },
+    { id: 1, description: "student" },
+    { id: 2, description: "unemployed" },
+    { id: 3, description: "employed" },
+  ];
 
   public contact = {
     name: "",
     isVIP: false,
     gender: "",
+    workStatus: 0,
+    company: "",
+    education: "",
   };
 
   constructor() {}
