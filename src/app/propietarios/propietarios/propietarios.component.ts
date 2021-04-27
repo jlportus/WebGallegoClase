ca58dad2d291869e53ced653d4edf4deee28feb9
JLPP <jl_puerto@hotmail.com>
2021-04-27 12:14:12 +0200

    Uso el servicio de acceso a la API y meto en comp

--- src/app/propietarios/propietarios/propietarios.component.ts
+++ src/app/propietarios/propietarios/propietarios.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Propietario } from "../models/propietario";
+import { PropietarioService } from "../services/propietario.service";
 
 @Component({
-  selector: 'app-propietarios',
-  templateUrl: './propietarios.component.html',
-  styles: []
+  selector: "app-propietarios",
+  templateUrl: "./propietarios.component.html",
+  styles: [],
 })
 export class PropietariosComponent implements OnInit {
+  public arrayPropietarios$: Propietario[] = [];
 
-  constructor() { }
+  constructor(private servicioPropietarios: PropietarioService) {}
 
   ngOnInit() {
+    this.servicioPropietarios.getPropietarios().subscribe((response) => {
+      response.forEach((p) => {
+        this.arrayPropietarios$.push(p);
+      });
+    });
+    console.log(`Estoy en el componente `)
+    console.log(this.arrayPropietarios$)
   }
-
 }
